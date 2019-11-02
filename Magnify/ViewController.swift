1f331e45779a74ba0d94d2318f49a3802a443e69
Tom Nelson <tkoinaustin@yahoo.com>
2019-11-02 08:11:22 -0500

    Initial Commit

---
+++ Magnify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magnify
+//
+//  Created by Tom Nelson on 11/2/19.
+//  Copyright © 2019 TKO Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
